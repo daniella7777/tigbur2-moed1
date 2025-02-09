ebfeddd003dd088f15a60184d41d2d77b43a6f32
daniella7777 <126858452+daniella7777@users.noreply.github.com>
2025-02-09 14:51:11 +0000

    changed a header

--- linked-list.h
+++ linked-list.h
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 
 typedef struct Node {
-    void* data;
-    struct Node* next;
+
 } Node_t;
 
 typedef struct LinkedList {
-    Node_t* head;
-    Node_t* tail;
+
 } List_t;
 
